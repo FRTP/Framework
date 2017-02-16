1ba123b9ed4d26edeb2a826c28177f2547d8de14
ndtretyak <ndtretyak@gmail.com>
2017-02-16 23:02:37 +0300

    autodownloading
    downloads all information to current directory

--- src/downloader.py
+++ src/downloader.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import timedelta
 from datetime import datetime
-from sys import argv
+from os import mkdir, chdir
 
 
 def download_file(url, local_filename, proxies):
@@ -12,17 +12,29 @@ def download_file(url, local_filename, proxies):
     return local_filename
 
 
-proxies = {
-  'http': 'http://92.242.44.43:8081'
-}
-name = argv[1]
-delta = timedelta(days=1)
-cur_date = datetime.today() - delta
-end_date = datetime(year=2015, month=1, day=1)
+def download_info(share_name):
+    proxies = {
+      'http': 'http://92.242.44.43:8081'
+    }
+    delta = timedelta(days=1)
+    cur_date = datetime.today() - delta
+    end_date = datetime(year=2015, month=1, day=1)
 
-while cur_date > end_date:
-    url = 'http://89.249.27.203:50011/papers/quik/tqbr/%s/%s/1000-1840/m1/csv'\
-          % (name, cur_date.strftime('20%y%m%d'))
-    local_filename = '%s/%s.csv' % (name, cur_date.strftime('20%y%m%d'))
-    download_file(url, local_filename, proxies)
-    cur_date -= delta
+    while cur_date > end_date:
+        url = 'http://89.249.27.203:50011/papers/quik/tqbr/%s/%s/1000-1840/m1/csv' % \
+              (share_name, cur_date.strftime('20%y%m%d'))
+        local_filename = cur_date.strftime('20%y%m%d.csv')
+        download_file(url, local_filename, proxies)
+        cur_date -= delta
+
+
+blue_chips = [
+    'GAZP', 'SBER', 'LKOH', 'SNGS', 'SNGSP', 'MGNT', 'NVTK', 'GMKN', 'VTBR',
+    'ROSN', 'TRNFP', 'TATN', 'MTSS', 'MOEX', 'ALRS', 'CHMF', 'SBERP', 'HYDR',
+    'RSTI', 'AFLT', 'BANE', 'IRAO', 'MAGN', 'NLMK', 'FEES'
+]
+for share_name in blue_chips:
+    mkdir(share_name)
+    chdir(share_name)
+    download_info(share_name)
+    chdir('..')
