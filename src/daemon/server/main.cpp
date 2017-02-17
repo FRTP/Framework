24110f2b48eb5e276fcfe13ea0b7dd2f8531eac1
maked0n <alex.mikhalevich@gmail.com>
2017-02-17 23:32:49 +0000

    Small restructuring

---
+++ src/daemon/server/main.cpp
@@ -0,0 +1,20 @@
+#include <stdexcept>
+#include <iostream>
+#include "cdaemon.h"
+#include "cparser.hpp"
+
+int main(int argc, char** argv) {
+	try {
+		CParser parser(argc, argv);
+		parser.parse();
+
+		CDaemon daemon(parser); 
+		if(daemon.start() != 0) 
+			std::cerr << "[EE]: Unable to start daemon" << std::endl;
+	}
+	catch(std::exception e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
