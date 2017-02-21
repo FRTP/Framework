973221f5c1b42918c28f4fd06d21241c77fdd753
Andrey Karyamin <algebroid.2011@gmail.com>
2017-02-21 12:28:03 +0300

    Added & edited some comments, added requirements.txt

--- src/ReportGenerator.py
+++ src/ReportGenerator.py
@@ -9,28 +9,31 @@ from reportlab.graphics import renderPDF
 from reportlab.graphics.charts.lineplots import LinePlot
 
 
+# Returns given canvas with text on it.
 def draw_text(input_canvas, text, x_margin, y_margin):
     input_canvas.drawString(x_margin, y_margin, text)
     return input_canvas
 
 
+# Returns Drawing instance which will be written to output pdf.
 def get_figure_drawing(input_data, x_margin, y_margin, width, height):
     time = np.arange(len(input_data))
     prefix_sums = np.cumsum(input_data)
 
-    # data in needed for Drawing format (pairs array)
+    # Data in needed for Drawing format (pairs array)
     data = [np.column_stack((time, prefix_sums))]
     data = [tuple(map(tuple, data[0]))]
 
-    drawing = Drawing(400, 200)
+    # Create drawing.
+    drawing = Drawing()
 
+    # Create graph.
     our_graph = LinePlot()
     our_graph.data = data
 
-    # ok
     our_graph.x = x_margin
     our_graph.y = y_margin
-    # ok
+
     our_graph.height = height
     our_graph.width = width
 
@@ -42,11 +45,11 @@ def get_figure_drawing(input_data, x_margin, y_margin, width, height):
 def generate_report(input_data, functors, path):
     the_canvas = canvas.Canvas(path, pagesize=landscape(A4))
 
-    # draw graph
+    # Draw graph to output pdf.
     figure_drawing = get_figure_drawing(input_data, 125, 300, 600, 250)
     renderPDF.draw(figure_drawing, the_canvas, 0, 0)
 
-    # write functor values
+    # Writing functors values to pdf file.
     shift_down = 0
     for functor in functors:
         name = functor.get_name()
