8cb9864d2cb02bf4bdebaaea33607dfe6f557214
Andrew <algebroid.2011@gmail.com>
2017-03-01 02:31:09 +0300

    Class environment added.

--- src/StockPortfolio.py
+++ src/StockPortfolio.py
@@ -3,38 +3,74 @@
 import numpy as np
 
 
-# Stock portfolio.
+class StockPortfolio:
+    def __init__(self, stock_count):
+        self.count = stock_count
+
+# Environment.
 # @brief
 #   Provides the interface to perform stocks manipulations.
 
 
-class StockPortfolio:
+class Environment:
 
-    # @param stocks_names - <list>
-    #   All stocks that considered by portfolio.
+    # PARAMS
     #
-    # @param stocks_count - <numpy array>
+    # @param historical_data
+    #   Historical data about prices on stocks in following format:
+    #   [(name, historical_prices, ( ... ), ... ], where :
+    #   - name is stock name
+    #   - historical_prices is np.array of history of cost of the stock.
+    #
+    # @param initial_stocks_count - <numpy array>
     #   The number of each stock we currently have.
     #
-    # @param stocks_prices - <numpy array>
+    # @param current_stocks_prices - <numpy array>
     #   The current price of each stock.
     #
-    # @param balance - <float>
+    # @param initial_balance - <float>
     #   Amount of money we currently have ( may be negative ).
     #
-    # @var coefficients ... todo
-    #   ...
 
-    def __init__(self, stocks_names, stocks_count, stocks_prices, balance):
-        self.names = stocks_names
-        self.count = np.array(stocks_count)
-        self.prices = stocks_prices
-        self.coefficients = self.gen_coefs()
-        self.current_balance = balance
+    # VARIABLES
+    #
+    # @var names
+    #   All stocks names we consider
+    #
+    # @var prices
+    #   Current prices of stocks we consider.
+    #
+    # @var current_balance
+    #
+    # @var balancing_coefficients
+    #
+    # @var current_balance
+    #   Amount of money we currently have.
+    #
+    # @var portfolio_sequence <StockPortfolio list>
+    #   Our trading history.
 
-    def gen_coefs(self):
-        # todo : gen somehow
-        raise NotImplementedError
+    def __init__(self, historical_data, initial_stocks_count,
+                 current_stocks_prices, initial_balance):
+        self.names = [name for (name, _) in historical_data]
+        self.prices = current_stocks_prices
+        self.current_balance = initial_balance
+        self.balancing_coefficients = \
+            self.compute_balancing_coefficients(historical_data)
+        stocks_count = np.array(initial_stocks_count)
+        self.portfolio_sequence = [StockPortfolio(stocks_count)]
+
+    # @brief
+    #   Computing balancing coefficients
+    # @param all_history_prices
+    #   Historical chronology of prices in such format:
+    #   [ (stock_name, np.array(prices history)), ( ... ), ... ]
+
+    @staticmethod
+    def compute_balancing_coefficients(historical_prices):
+        means = np.array([prices.mean() for (_, prices) in historical_prices])
+        max_mean = np.max(means)
+        return means/float(max_mean)
 
     # Buy stocks
     # @brief
@@ -50,25 +86,27 @@ class StockPortfolio:
     #   New instance of updated StockPortfolio if succeeded.
 
     def buy(self, given_names, given_count):
-        new_count = self.count
+        new_stocks_count = self.portfolio_sequence[-1].count
         new_balance = self.current_balance
 
         if len(given_names) != len(given_count):
             raise Exception('given_names and given_count '
                             'must have the same size')
+        if given_names.empty():
+            raise Exception("given_names mustn't be empty")
 
         for i in range(len(given_names)):
-            index = self.names.index(given_names[i])
-            new_count[index] += 1
-            new_balance -= self.prices[index] * given_count[i]
+            current_stock_index = self.names.index(given_names[i])
+            new_stocks_count[current_stock_index] += given_count[i]
+            new_balance -= self.prices[current_stock_index] * given_count[i]
 
-        return StockPortfolio(stocks_names=self.names,
-                              stocks_count=new_count,
-                              stocks_prices=self.prices,
-                              balance=new_balance)
+        new_portfolio = StockPortfolio(new_stocks_count)
+        self.portfolio_sequence.append(new_portfolio)
+
+        return new_portfolio
 
     # @brief
-    #   Sell stocks and get updated StockPortfolio
+    #   Sell stocks and returns updated StockPortfolio
     # @param given_names - <list>
     #   Names of stocks to buy. Size must be equal to given_count size.
     # @param given_count - <numpy array>
@@ -85,4 +123,11 @@ class StockPortfolio:
     #   The amount of a particular stock we currently have.
 
     def get_stock_count(self, stock_name):
-        return self.count[self.names.index(stock_name)]
+        needed_index = self.names.index(stock_name)
+        return self.portfolio_sequence[-1].count[needed_index]
+
+    # @brief
+    #   Get index of a stock_name
+    #   corresponding to portfolio's count index
+    def stock_index(self, stock_name):
+        return self.names.index(stock_name)
