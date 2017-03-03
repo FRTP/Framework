8bdbf05870e58828db77537fe15674157cc1b253
maked0n <alex.mikhalevich@gmail.com>
2017-03-03 21:54:08 +0000

    Auxiliary script

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,14 +1,20 @@
 import os
 import calendar
+import csv
+
 
 working_dir = "/var/frtp/data/"
 dir_list = os.listdir(working_dir)
+deleted_num = 0
 for directory in dir_list:
     cur_list = os.listdir(working_dir + directory)
     for fl in cur_list:
-        year = int(fl[:4])
-        month = int(fl[4:6])
-        day = int(fl[6:8])
-        weekday = calendar.weekday(year, month,  day)
-        if weekday == 5 or weekday == 6:
-            os.remove(working_dir + directory + "/" + fl)
+        full_name = working_dir + directory + "/" + fl
+        with open(full_name, 'r') as csv_file:
+            next(csv_file)
+            csv_reader = csv.reader(csv_file, delimiter=";")
+            volume = sum(int(r[5]) for r in csv_reader)
+            if volume == 0:
+                os.remove(full_name)
+                deleted_num += 1
+print("Deleted " + str(deleted_num) + " files\n")
