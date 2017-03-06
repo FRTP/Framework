1392eaf0278670990b2cf57b6bb9a27f0250b4d8
maked0n <alex.mikhalevich@gmail.com>
2017-03-06 17:46:07 +0000

    Build system updates

--- src/build.sh
+++ src/build.sh
@@ -1,31 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $1 = "help" ]; then
-	echo "Usage: sh build.sh <target> [options]"
+function usage { 
+	echo "Usage: sh build.sh -t <target> [options]"
 	echo -e "\n"
 	echo "Targets:"
 	echo -e "all \t build all targets"
 	echo -e "daemon \t build server daemon and client lib"
 	echo -e "server \t build server part"
 	echo -e "client \t build client lib"
+	echo -e "clean \t clean build directory"
 	echo -e "\n"
 	echo "Options:"
-	echo -e "--enable-testing \t  build with tests"
-	echo -e "--debug \t\t  build debug version"
-fi
+	echo -e "-s \t build with tests"
+	echo -e "-d \t build debug version"
+	echo -e "-h \t show this help"
+	exit 1
+}
 
-if [ -n "$2" ]; then
-	if [ $2 = "--enable-testing" ]; then
-		echo Testing is not implemented yet
-		exit 127
-	elif [ $2 = "--debug" ]; then
-		echo Building debug version
-		make $1 CFLAGS=-g
-	else
-		echo Unknown option, use 'sh build.sh help' for more details
-	fi 
-else
-	if [ $1 != "help" ]; then
-		make $1
-	fi
-fi
+TARGET=""
+ADDITIONAL_CFLAGS=""
+
+while getopts "t:sdh" opt;
+do
+	case $opt in
+		t)
+			case $OPTARG in
+				"") 	echo Target was not specified, aborting
+					exit 127
+					;;
+				*)	if [ $OPTARG = "daemon" ] || [ $OPTARG = "server" ] \
+					|| [ $OPTARG = "client" ] || [ $OPTARG = "clean" ] \
+					|| [ $OPTARG = "all" ]; then
+						TARGET="$OPTARG"
+					else
+						echo Unknown target, aborting
+						exit 127
+					fi
+					;;
+			esac
+			;;
+		s)
+			echo Testing is not implemented yet
+			exit 127
+			;;
+		d)
+			ADDITIONAL_CFLAGS+=-g
+			echo Building debug version
+			;;
+		h) 
+			usage
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+
+make $TARGET CFLAGS=$ADDITIONAL_CFLAGS
