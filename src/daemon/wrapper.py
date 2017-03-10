2203e9906b727df433d55c95e7f0b64a96ef7cfa
Alexander Mikhalevich <alex.mikhalevich@gmail.com>
2017-03-10 14:57:39 +0300

    Style fixes

--- src/daemon/wrapper.py
+++ src/daemon/wrapper.py
@@ -44,8 +44,8 @@ class CNoConversion(IConverter):
 
 class CClient(object):
     def __init__(self, srv_address, srv_port, working_dir):
-        self.client = lib.LibClient(server=srv_address, port=srv_port,
-                                    workingdir=working_dir)
+        self.client = lib.LibClient(server = srv_address, port = srv_port,
+                                    workingdir = working_dir)
         self.context = self.client.create_context()
         lib.LibClient.connect(context=self.context)
         if not os.path.exists(working_dir):
@@ -53,30 +53,30 @@ class CClient(object):
 
     def get_file(self, filename, newfilename, data_type, force=False):
         l_arg = [filename, newfilename]
-        cmd = lib.LibCommandFactory.create_command(cmd_id="GetFile",
-                                                   args=l_arg,
-                                                   str(force)])
-        return lib.LibClient.invoke(context=self.context, command=cmd,
-                                    datatype=DATA_TYPES[data_type])
+        cmd = lib.LibCommandFactory.create_command(cmd_id = "GetFile",
+                                                   args = l_arg,
+                                                   str(force))
+        return lib.LibClient.invoke(context = self.context, command = cmd,
+                                    datatype = DATA_TYPES[data_type])
     
     def upload_file(self, filename, data_type):
-        cmd = lib.LibCommandFactory.create_command(cmd_id="UploadFile",
-                                                   args=[filename])
-        return lib.LibClient.invoke(connect=self.context, command=cmd,
-                                    datatype=DATA_TYPES[data_type])
+        cmd = lib.LibCommandFactory.create_command(cmd_id = "UploadFile",
+                                                   args = [filename])
+        return lib.LibClient.invoke(connect = self.context, command = cmd,
+                                    datatype = DATA_TYPES[data_type])
 
     def get_md5(self, srv_filename, data_type):
-        cmd = lib.LibCommandFactory.create_command(cmd_id="GetMD5",
-                                                   args=[srv_filename])
-        lib.LibClient.invoke(context=self.context, command=cmd,
-                             datatype=DATA_TYPES[data_type])
+        cmd = lib.LibCommandFactory.create_command(cmd_id = "GetMD5",
+                                                   args = [srv_filename])
+        lib.LibClient.invoke(context = self.context, command = cmd,
+                             datatype = DATA_TYPES[data_type])
         return CClient.get_hash(cmd)
 
     def check_integrity(self, srv_filename, cli_filename, data_type):
-        return lib.LibClient.check_integrity(context=self.context,
-                                             server=srv_filename,
-                                             client=cli_filename,
-                                             datatype=DATA_TYPES[data_type])
+        return lib.LibClient.check_integrity(context = self.context,
+                                             server = srv_filename,
+                                             client = cli_filename,
+                                             datatype = DATA_TYPES[data_type])
 
     def get_info(self, converter, check=True, force=False):
         for filename in converter.get_filenames():
