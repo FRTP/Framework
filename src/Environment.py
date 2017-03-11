767c0c93d1fa9433624e147097e84b0cdaba1c81
avk <algebroid.2011@gmail.com>
2017-03-11 13:12:53 +0300

    replaced 'stock -> asset'

--- src/Environment.py
+++ src/Environment.py
@@ -4,22 +4,22 @@ from datetime import datetime
 import numpy as np
 
 
-class StockPortfolio:
+class AssetsPortfolio:
     # @throws ValueError
-    #   If current_date has wrong format (not StockPortfolio.TIME_FORMAT)
+    #   If current_date has wrong format (not AssetsPortfolio.TIME_FORMAT)
     #
     # @throws TypeError
     #   If current_date is not <str> or <datetime>
     #
-    def __init__(self, stock_count, current_date):
-        self.count = stock_count
-        self.date = StockPortfolio.to_datetime(current_date)
+    def __init__(self, assets_count, current_date):
+        self.count = assets_count
+        self.date = AssetsPortfolio.to_datetime(current_date)
 
     # @brief
-    # @param all_stocks_names <np array of 'str'>
-    # @param stock_name
-    def get_stock_count(self, all_stocks_names, stock_name):
-        index = all_stocks_names.tolist().index(stock_name)
+    # @param all_assets_names <np array of 'str'>
+    # @param asset_name
+    def get_asset_count(self, all_assets_names, asset_name):
+        index = all_assets_names.tolist().index(asset_name)
         return self.count[index]
 
     TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
@@ -32,29 +32,29 @@ class StockPortfolio:
         if type(given_date) == str:
             try:
                 given_date = datetime.strptime(given_date,
-                                               StockPortfolio.TIME_FORMAT)
+                                               AssetsPortfolio.TIME_FORMAT)
             except ValueError:
-                raise ValueError(StockPortfolio.ERR_MSG_WRONG_TIME_FORMAT)
+                raise ValueError(AssetsPortfolio.ERR_MSG_WRONG_TIME_FORMAT)
 
         elif type(given_date) != datetime:
-            raise TypeError(StockPortfolio.ERR_MSG_WRONG_TIME_TYPE)
+            raise TypeError(AssetsPortfolio.ERR_MSG_WRONG_TIME_TYPE)
 
         return given_date
 
 
 # Environment.
 # @brief
-#   Provides the interface to perform stocks manipulations.
+#   Provides the interface to perform assets manipulations.
 
 
 class Environment:
     # VARIABLES
     #
     # @var names
-    #   All stocks names we consider
+    #   All assets names we consider
     #
     # @var prices
-    #   Current prices of stocks we consider.
+    #   Current prices of assets we consider.
     #
     # @var current_balance
     #
@@ -63,13 +63,13 @@ class Environment:
     # @var current_balance
     #   Amount of money we currently have.
     #
-    # @var portfolio_sequence <StockPortfolio list>
+    # @var portfolio_sequence <AssetsPortfolio list>
     #   Our trading history.
 
     # @params
     #
     # @param historical_data <pandas DataFrame>
-    #   Historical data about prices on stocks in following format:
+    #   Historical data about prices on assets in following format:
     #    ___________________________________________________
     #   | Date & Time            | 'Sber' | 'Gazprom' | ... |
     #   | -----------------------|--------|-----------|-----|
@@ -79,33 +79,33 @@ class Environment:
     #
     #
     #
-    # @param initial_stocks_count - <numpy array>
-    #   The number of each stock we currently have.
+    # @param initial_assets_count - <numpy array>
+    #   The number of each asset we currently have.
     #
-    # @param current_stocks_prices - <numpy array>
-    #   The current price of each stock.
+    # @param current_assets_prices - <numpy array>
+    #   The current price of each asset.
     #
     # @param initial_balance - <float>
     #   Amount of money we currently have ( may be negative ).
     #
-    def __init__(self, hist_data, initial_stocks_count=None,
-                 current_stocks_prices=None, initial_date=None,
+    def __init__(self, hist_data, initial_assets_count=None,
+                 current_assets_prices=None, initial_date=None,
                  initial_balance=0):
 
-        if initial_stocks_count is None:
-            initial_stocks_count = []
-        if current_stocks_prices is None:
-            current_stocks_prices = []
+        if initial_assets_count is None:
+            initial_assets_count = []
+        if current_assets_prices is None:
+            current_assets_prices = []
         if initial_date is None:
             initial_date = ""
 
         self.names = np.array(hist_data.columns)[1:]
-        self.prices = current_stocks_prices
+        self.prices = current_assets_prices
         self.current_balance = initial_balance
         self.balancing_coefficients = \
             self.compute_balancing_coefficients(hist_data)
-        stocks_count = np.array(initial_stocks_count)
-        self.portfolio_sequence = [StockPortfolio(stocks_count, initial_date)]
+        assets_count = np.array(initial_assets_count)
+        self.portfolio_sequence = [AssetsPortfolio(assets_count, initial_date)]
 
     # @brief
     #   Computing balancing coefficients
@@ -123,15 +123,15 @@ class Environment:
 
         return np.array(coefficients)
 
-    # Buy stocks
+    # Buy assets
     # @brief
-    #   Buy stocks and get updated StockPortfolio.
+    #   Buy assets and get updated AssetsPortfolio.
     #
     # @param given_names - <list>
-    #   Names of stocks to buy. Size must be equal to given_count size.
+    #   Names of assets to buy. Size must be equal to given_count size.
     #
     # @param given_count - <numpy array>
-    #   Count of stocks to buy. Size must be equal to given_names size.
+    #   Count of assets to buy. Size must be equal to given_names size.
     #
     # @param purchase_date <datetime>
     #   Date of the purchase
@@ -140,10 +140,10 @@ class Environment:
     #   if len(given_names) != len(given_count)
     #
     # @returns
-    #   New instance of updated StockPortfolio if succeeded.
+    #   New instance of updated AssetsPortfolio if succeeded.
     #
     def buy(self, given_names, given_count, purchase_date):
-        new_stocks_count = self.portfolio_sequence[-1].count
+        new_assets_count = self.portfolio_sequence[-1].count
 
         if len(given_names) != len(given_count):
             raise Exception('given_names and given_count '
@@ -152,94 +152,94 @@ class Environment:
             raise Exception("given_names mustn't be empty")
 
         for i in range(len(given_names)):
-            current_stock_idx = self.names.tolist().index(given_names[i])
-            current_coef = self.balancing_coefficients[current_stock_idx]
-            new_stocks_count[current_stock_idx] += \
+            current_asset_idx = self.names.tolist().index(given_names[i])
+            current_coef = self.balancing_coefficients[current_asset_idx]
+            new_assets_count[current_asset_idx] += \
                 given_count[i] * current_coef
             self.current_balance -= \
-                self.prices[current_stock_idx] * given_count[i] * current_coef
+                self.prices[current_asset_idx] * given_count[i] * current_coef
 
-        new_portfolio = StockPortfolio(new_stocks_count, purchase_date)
+        new_portfolio = AssetsPortfolio(new_assets_count, purchase_date)
         self.portfolio_sequence.append(new_portfolio)
 
         return new_portfolio
 
     # @brief
-    #   Sell stocks and returns updated StockPortfolio
+    #   Sell assets and returns updated AssetsPortfolio
     #
     # @param given_names - <list>
-    #   Names of stocks to buy. Size must be equal to given_count size.
+    #   Names of assets to buy. Size must be equal to given_count size.
     #
     # @param given_count - <numpy array>
-    #   Count of stocks to buy. Size must be equal to given_names size.
+    #   Count of assets to buy. Size must be equal to given_names size.
     #
     def sell(self, given_names, given_count, purchase_date):
         return self.buy(given_names, -given_count, purchase_date)
 
     # @brief
-    #   Getting count of a particular stock we have.
+    #   Getting count of a particular asset we have.
     #
-    # @param stock_name
-    #   The name of a stock we want.
+    # @param asset_name
+    #   The name of a asset we want.
     #
     # @returns
-    #   The amount of a particular stock we currently have.
+    #   The amount of a particular asset we currently have.
     #
-    def get_stock_count(self, stock_name):
-        needed_index = self.names.tolist().index(stock_name)
+    def get_asset_count(self, asset_name):
+        needed_index = self.names.tolist().index(asset_name)
         return self.portfolio_sequence[-1].count[needed_index]
 
     # @brief
-    #   Updates price of the one stock
-    def update_price(self, stock, new_price):
-        index = self.names.tolist().index(stock)
+    #   Updates price of the one asset
+    def update_price(self, asset, new_price):
+        index = self.names.tolist().index(asset)
         self.prices[index] = new_price
 
     # @brief
-    #   Updates prices of given stocks
+    #   Updates prices of given assets
     #
-    # @param stocks_names <numpy array>
-    #   Array of stocks names to be updated.
+    # @param assets_names <numpy array>
+    #   Array of assets names to be updated.
     #
     # @param new_prices <numpy array>
-    #   Array of new prices of the stocks to be updated.
+    #   Array of new prices of the assets to be updated.
     #
     # @throws Exception
-    #   If len(stocks_names) != len(new_prices) throws an Exception.s
+    #   If len(assets_names) != len(new_prices) throws an Exception.s
     #
-    def update_prices(self, stocks_names, new_prices):
-        if len(stocks_names) != len(new_prices):
-            raise Exception("stocks_names & new_prices "
+    def update_prices(self, assets_names, new_prices):
+        if len(assets_names) != len(new_prices):
+            raise Exception("assets_names & new_prices "
                             "must have the same size")
-        for i in range(len(stocks_names)):
-            self.update_price(stocks_names[i], new_prices[i])
+        for i in range(len(assets_names)):
+            self.update_price(assets_names[i], new_prices[i])
 
     # @brief
-    #   Extracts needed_stock's price at needed_date
+    #   Extracts needed_asset's price at needed_date
     #
-    # @param needed_stock <str>
-    #   Needed stock name.
+    # @param needed_asset <str>
+    #   Needed asset name.
     #
     # @param needed_date <str> in following format :%Y-%m-%dT%H:%M:%S
     #    or <datetime>.
     #
     # @throws ValueError
-    #   If needed_date has wrong format (not StockPortfolio.TIME_FORMAT)
+    #   If needed_date has wrong format (not AssetsPortfolio.TIME_FORMAT)
     #
     # @throws TypeError
     #   If needed_date is not <str> or <datetime>
     #
     # @returns
-    #   Needed stock price - if found.
+    #   Needed asset price - if found.
     #   None - if wasn't found.
     #
     @staticmethod
-    def get_price_by_date(hist_data, needed_stock, needed_date):
-        needed_date = StockPortfolio.to_datetime(needed_date)
+    def get_price_by_date(hist_data, needed_asset, needed_date):
+        needed_date = AssetsPortfolio.to_datetime(needed_date)
         idx = Environment.__find__row__by__date__(hist_data, needed_date)
 
         if idx is not None:
-            return hist_data[needed_stock][idx]
+            return hist_data[needed_asset][idx]
         else:
             return None
 
@@ -249,7 +249,7 @@ class Environment:
             raise TypeError("given_date must be <datetime> type.")
 
         for i in range(len(hist_data)):
-            cur_date = StockPortfolio.to_datetime(hist_data[:, 0][i])
+            cur_date = AssetsPortfolio.to_datetime(hist_data[:, 0][i])
             if cur_date == given_date:
                 return i
         return None
@@ -262,7 +262,7 @@ class Environment:
         if start_date is None:
             first_idx = 0
         else:
-            start_date = StockPortfolio.to_datetime(start_date)
+            start_date = AssetsPortfolio.to_datetime(start_date)
             first_idx = Environment.__find__row__by__date__(hist_data,
                                                             start_date)
             if first_idx is None:
@@ -274,7 +274,7 @@ class Environment:
 
         return hist_data[first_idx:last_idx]
 
-    def get_stocks_names(self):
+    def get_assets_names(self):
         return self.names
 
     def get_current_balance(self):
--- src/EnvironmentUsageExample.py
+++ src/EnvironmentUsageExample.py
@@ -10,48 +10,48 @@ historical_data = pd.read_csv('historical_data_example.csv')
 
 
 # Generate some initials for environment.
-stocks_names = historical_data.columns[1:]
-initial_stocks_count = np.arange(len(stocks_names))
-current_stocks_prices = np.linspace(30, 90, len(stocks_names))
+assets_names = historical_data.columns[1:]
+initial_assets_count = np.arange(len(assets_names))
+current_assets_prices = np.linspace(30, 90, len(assets_names))
 initial_balance = 100500
 
 
 print("Initial balance :", initial_balance)
-print("Stocks names :", stocks_names)
-print("Stocks prices :", current_stocks_prices)
-print("Stocks count :", initial_stocks_count)
+print("assets names :", assets_names)
+print("assets prices :", current_assets_prices)
+print("assets count :", initial_assets_count)
 print(historical_data)
 print()
 
 # Create environment with some initials
-our_environment = Environment(historical_data, initial_stocks_count,
-                              current_stocks_prices, "1994-11-05T14:12:30",
+our_environment = Environment(historical_data, initial_assets_count,
+                              current_assets_prices, "1994-11-05T14:12:30",
                               initial_balance)
 
 
-# Print all stocks we currently have according to the environment
-def print_stocks_we_have(env):
-    names = env.get_stocks_names()
+# Print all assets we currently have according to the environment
+def print_assets_we_have(env):
+    names = env.get_assets_names()
     for name in names:
-        print("{} count: {}".format(name, env.get_stock_count(name)))
+        print("{} count: {}".format(name, env.get_asset_count(name)))
     print("Current balance : {}".format(env.get_current_balance()))
     print("-----------------------")
     return
 
 
-# Print all stocks
-print_stocks_we_have(our_environment)
+# Print all assets
+print_assets_we_have(our_environment)
 
-# Buy some stocks.
+# Buy some assets.
 our_environment.buy(["Sberbank", "Gazprom"], np.array([18, 10]),
                     "1994-11-05T13:15:30")
 
-# Print all stocks
-print_stocks_we_have(our_environment)
+# Print all assets
+print_assets_we_have(our_environment)
 
-# Sell some stocks.
+# Sell some assets.
 our_environment.sell(["Gazprom", "Rostelecom"], np.array([7, 6]),
                      "1994-11-05T14:15:30")
 
-# Print all stocks
-print_stocks_we_have(our_environment)
+# Print all assets
+print_assets_we_have(our_environment)
