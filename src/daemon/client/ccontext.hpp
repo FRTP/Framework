6d4b5d07763e3aa9a54d1af1947e707ec08194ec
maked0n <alex.mikhalevich@gmail.com>
2017-03-13 18:37:04 +0000

    Bugfixes: 1. Annoying typo 2. Prevent strings from being sent char-by-char

--- src/daemon/client/ccontext.hpp
+++ src/daemon/client/ccontext.hpp
@@ -21,18 +21,21 @@ class CContext {
 	public:
 		CContext(const std::string& server, int port, boost::shared_ptr<io_service> io_service)
 			: m_server(server), m_port(port), m_socket(*(io_service.get())) {}
+
 		void connect(boost::system::error_code& error) {
 			m_socket.connect(ip::tcp::endpoint(ip::address::from_string(m_server.c_str()), m_port), error);
 		}
+
 		bool socket_opened() const {
 			return m_socket.is_open();
 		}
+
 		template<class T>
 		EError socket_write(const T& msg) {
 			boost::system::error_code error;
 			std::ostream out(&m_writebuf);
 			for (auto i : msg) {
-				out << msg[i];
+				out << i << " ";
 			}
 			out << std::endl;
 			write(m_socket, m_writebuf, error);
@@ -42,6 +45,7 @@ class CContext {
 			}
 			return EError::OK;
 		}
+
 		EError socket_read(streambuf& buffer) {
 			boost::system::error_code error;
 			read_until(m_socket, buffer, '\n', error);
@@ -50,9 +54,23 @@ class CContext {
 			}
 			return EError::OK;
 		}
+
 		~CContext() {
 			m_socket.close();
 		}
 };
+
+template<>
+inline EError CContext::socket_write<std::string>(const std::string& msg) {
+	boost::system::error_code error;
+	std::ostream out(&m_writebuf);
+	out << msg << std::endl;
+	write(m_socket, m_writebuf, error);
+	m_writebuf.consume(m_writebuf.size());
+	if (error) {
+		return EError::WRITE_ERROR;
+	}
+	return EError::OK;
+}
 
 #endif //CCONTEXT_HPP
