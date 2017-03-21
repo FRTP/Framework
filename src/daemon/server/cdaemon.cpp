cb21ab98493323741f74c9054ad770037c684f14
maked0n <alex.mikhalevich@gmail.com>
2017-03-21 20:53:51 +0000

    Refactor

--- src/daemon/server/cdaemon.cpp
+++ src/daemon/server/cdaemon.cpp
@@ -1,14 +1,13 @@
 #include "cdaemon.h"
 
-CServer::CServer(io_service& io_service)
-	: m_acceptor(io_service, ip::tcp::endpoint(ip::tcp::v4(), PORT)) {
-
+CServer::CServer(boost::shared_ptr<io_service> io_srvs)
+	: m_acceptor(*io_srvs, ip::tcp::endpoint(ip::tcp::v4(), PORT)) {
 	CDataTypeFactory::register_type<CDataTypeShares>(EDataType::SHARES);
 	CDataTypeFactory::register_type<CDataTypeTwitter>(EDataType::TWITTER);
 
-	CCommandFactory::add<CCmdGetFile>(ECommand::GET_FILE);
-	CCommandFactory::add<CCmdGetMD5>(ECommand::GET_MD5);
-	CCommandFactory::add<CCmdUploadFile>(ECommand::UPLOAD_FILE);
+	CCommandFactory::add<server_command::CCmdGetFile>(ECommand::GET_FILE);
+	CCommandFactory::add<server_command::CCmdGetMD5>(ECommand::GET_MD5);
+	CCommandFactory::add<server_command::CCmdUploadFile>(ECommand::UPLOAD_FILE);
 
 	_start_accept();	
 }
@@ -16,81 +15,25 @@ CServer::CServer(io_service& io_service)
 void CServer::_start_accept() {
 	BOOST_LOG_TRIVIAL(info) << "Waiting for client...";
 
-	CTCPConnection::conn_ptr connection = CTCPConnection::create(m_acceptor.get_io_service());
-	m_acceptor.async_accept(connection->m_socket, boost::bind(&CServer::_handle_accept, this, connection, 
+	CTCPConnection::conn_ptr connection = CTCPConnection::conn_ptr(new CTCPConnection(m_acceptor.get_io_service()));
+	m_acceptor.async_accept(connection->context().socket(), boost::bind(&CServer::_handle_accept, this, connection,
 				placeholders::error));
 }
 
 void CServer::_handle_accept(CTCPConnection::conn_ptr connection, const boost::system::error_code& ec) {
 	BOOST_LOG_TRIVIAL(info) << "Accepting client...";
 	if (!ec) {
-		BOOST_LOG_TRIVIAL(info) << "New client accepted; reading command...";
-		async_read_until(connection->m_socket, connection->m_readbuf, "\n",
-				 boost::bind(&CTCPConnection::handle_read_command, connection,
-				 placeholders::error));
+		BOOST_LOG_TRIVIAL(info) << "New client accepted";
+		while (connection->context().socket_opened()) {
+			BOOST_LOG_TRIVIAL(info) << "Reading command...";
+			connection->context().async_recv_message(boost::bind(&CTCPConnection::handle_recv_message, connection,
+								 placeholders::error));
+		}
 	}
 	else {
 		BOOST_LOG_TRIVIAL(error) << "Unable to accept client: " + ec.message();
 	}
 	_start_accept();
-}
-
-void CTCPConnection::_send_feedback(EError error) {
-	BOOST_LOG_TRIVIAL(info) << "Sending feedback...";
-	std::vector<char> payload({ static_cast<char>(error) });
-	CMessage feedback(ECommand::FEEDBACK, EDataType::ERROR_CODE, payload);
-	feedback.to_streambuf(m_writebuf);
-	async_write(m_socket, m_writebuf,
-		    boost::bind(&CTCPConnection::handle_write_response, shared_from_this(),
-		    placeholders::error));
-}
-
-void CTCPConnection::handle_recv_message(const boost::system::error_code& ec) {
-	if (!ec || ec == error::eof) {
-		BOOST_LOG_TRIVIAL(info) << "Processing incoming message...";
-		boost::shared_ptr<CMessage> msg(new CMessage);
-		EError ret = msg->from_streambuf(m_readbuf);
-		m_readbuf.consume(m_readbuf.size());
-		if (ret != EError::OK) {
-			BOOST_LOG_TRIVIAL(warning) << get_text_error(ret);
-			_send_feedback(ret);
-			return;
-		}
-		_process_message(msg);
-	}
-	else {
-		BOOST_LOG_TRIVIAL(error) << "handle_recv_message: " + ec.message();
-	}
-}
-
-void CTCPConnection::_process_message(boost::shared_ptr<CMessage> msg) {
-	auto cmd = CCommandFactory::create(*msg);
-	if (cmd == NULL) {
-		_send_feedback(EError::UNKNOWN_COMMAND);
-		return;
-	}
-	cmd->extract_args_from_message(*msg);
-	EError ret = cmd->invoke(m_context, msg->datatype());
-	if (ret != EError::OK) {
-		m_context->async_send_feedback(ret);
-	}
-	if (cmd) {
-		delete cmd;
-	}
-}
-
-void CTCPConnection::handle_write_response(const boost::system::error_code& ec) {
-	m_writebuf.consume(m_writebuf.size());
-	if (!ec) { 
-		BOOST_LOG_TRIVIAL(info) << "Response transmitted successfully";
-	}
-	else {
-		BOOST_LOG_TRIVIAL(error) << "handle_write_response: " +  ec.message();
-	}
-}
-
-CTCPConnection::conn_ptr CTCPConnection::create(io_service& io_service) {
-	return conn_ptr(new CTCPConnection(io_service));
 }
 
 CDaemon::CDaemon(const CParser& parser) : m_io_service(new io_service()) {
@@ -146,7 +89,7 @@ int CDaemon::start() {
 	m_io_service->notify_fork(io_service::fork_child);
 
 	BOOST_LOG_TRIVIAL(info) << "CDaemon::start(): daemon started.";
-	CServer server(*(m_io_service.get()));
+	CServer server(m_io_service);
 	m_io_service->run();
 	BOOST_LOG_TRIVIAL(info) << "CDaemon::start(): daemon stopped.";
 
---
+++ src/daemon/server/ctcpconnection.hpp
@@ -0,0 +1,65 @@
+#ifndef CTCPCONNECTION_HPP
+#define CTCPCONNECTION_HPP
+
+#include <boost/pointer_cast.hpp>
+
+#include "ccommand.h"
+#include "../ccontext.hpp"
+
+class CTCPConnection : public boost::enable_shared_from_this<CTCPConnection> {
+	private:
+		CContext m_context;
+
+		void _process_message(boost::shared_ptr<CMessage> msg) {
+			auto cmd = boost::dynamic_pointer_cast<server_command::IServerCommand>(CCommandFactory::create(*msg));
+			if (cmd == NULL) {
+				m_context.async_send_feedback(EError::UNKNOWN_COMMAND, 
+							      boost::bind(&CTCPConnection::handle_write_response, 
+							      shared_from_this(), placeholders::error));
+				return;
+			}
+			cmd->set_callback(boost::bind(&CTCPConnection::handle_write_response, shared_from_this(),
+					  placeholders::error));
+			EError ret = cmd->invoke(&m_context, msg->datatype());
+			if (ret != EError::OK) {
+				m_context.async_send_feedback(ret, boost::bind(&CTCPConnection::handle_write_response, 
+							      shared_from_this(), placeholders::error));
+			}
+		}
+	public:
+		typedef boost::shared_ptr<CTCPConnection> conn_ptr;
+
+		explicit CTCPConnection(io_service& io_srvs) 
+			: m_context(io_srvs) {}
+
+		void handle_recv_message(const boost::system::error_code& ec) {
+			if (!ec || ec == error::eof) {
+				BOOST_LOG_TRIVIAL(info) << "Processing incoming message...";
+				boost::shared_ptr<CMessage> msg(new CMessage);
+				m_context.read_buffer(*msg);
+				_process_message(msg);
+			}
+			else {
+				BOOST_LOG_TRIVIAL(error) << "handle_recv_message: " + ec.message();
+				m_context.async_send_feedback(EError::INTERNAL_ERROR, 
+							      boost::bind(&CTCPConnection::handle_write_response, 
+							      shared_from_this(), placeholders::error));
+			}
+		}
+
+		void handle_write_response(const boost::system::error_code& ec) {
+			m_context.wflush();
+			if (!ec) { 
+				BOOST_LOG_TRIVIAL(info) << "Response transmitted successfully";
+			}
+			else {
+				BOOST_LOG_TRIVIAL(error) << "handle_write_response: " +  ec.message();
+			}
+		}
+
+		CContext& context() {
+			return m_context;
+		}
+};
+
+#endif //CTCPCONNECTION_HPP
