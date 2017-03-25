9ebaf5029c6ef5d53054d37cc67082a1d750b6eb
maked0n <alex.mikhalevich@gmail.com>
2017-03-25 17:07:24 +0000

    More thorough error handling

--- src/daemon/client/cclient.cpp
+++ src/daemon/client/cclient.cpp
@@ -11,6 +11,7 @@ CClient::CClient(const std::string& working_dir) {
 	CDataTypeFactory::register_type<CDataTypeTwitter>(EDataType::TWITTER);
 
 	CSettings::set_working_dir(working_dir);
+	CSettings::set_data_dir("data/");
 }
 
 CContext* CClient::create_context() {
@@ -56,12 +57,26 @@ std::string CClient::get_hash(CCmdGetMD5* cmd) {
 
 bool CClient::check_integrity(CContext* context, const std::string& srv_filename,
 			      const std::string& cli_filename, int datatype) {
-	boost::shared_ptr<CCmdGetMD5> cmd = boost::shared_ptr<CCmdGetMD5>(new CCmdGetMD5(std::list<std::string>({ srv_filename })));
+	auto cmd = boost::shared_ptr<CCmdGetMD5>(new CCmdGetMD5(std::list<std::string>{ srv_filename }));
+	if (cmd == nullptr) {
+		throw ExError("Unable to create GetMD5 command", "CClient::check_integrity()");
+	}
 	invoke(context, cmd.get(), datatype);
 	md5sum_ptr srv_md5_hash = cmd->hash();
+	if (srv_md5_hash == nullptr) {
+		throw ExError("Unable to correctly obtain MD5 from server", "CClient::check_integrity()");
+	}
 
-	std::string full_path(CSettings::working_dir() + get_data_type_dir(static_cast<EDataType>(datatype)) + "/" + cli_filename);
+	std::string full_path = get_full_path(static_cast<EDataType>(datatype), cli_filename);
 	md5sum_ptr cli_md5_hash = calculate_md5(full_path);
+	if (cli_md5_hash == nullptr) {
+		throw ExError("Unable to calculate MD5", "CClient::check_integrity()");
+	}
+
+	if (srv_md5_hash->size() != MD5_DIGEST_LENGTH ||
+	    cli_md5_hash->size() != MD5_DIGEST_LENGTH) {
+		return false;
+	}
 	for (unsigned int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
 		if ((*srv_md5_hash)[i] != (*cli_md5_hash)[i]) {
 			return false;
